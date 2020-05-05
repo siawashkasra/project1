137dd4e268e2f542c854c85048244b8cda1c493f
siawashkasra <siawashkasra@gmail.com>
2020-05-05 17:22:39 +0430

    Improved code for session and view

--- application.py
+++ application.py
@@ -16,11 +16,6 @@ app.secret_key = b'\x11\x1d\\>\x1a3\x806N\xb1\x1fk=\xe39\xb5'
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
 # # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
