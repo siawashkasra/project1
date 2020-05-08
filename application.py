e6722e0601733217fed4df3a5eeba85b35a67992
siawashkasra <siawashkasra@gmail.com>
2020-05-08 15:50:06 +0430

    Refactored and Improved the code

--- application.py
+++ application.py
@@ -6,64 +6,57 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from flask import render_template, request
-from flask_login import LoginManager, login_user, logout_user
 from flask_wtf.csrf import CSRFProtect
 from forms import RegistrationForm, LoginForm, ReviewForm
+from controller import Controller
+
 
 
 
 app = Flask(__name__)
-# login_manager = LoginManager()
-# login_manager.init_app(app)
 csrf = CSRFProtect(app)
 KEY = "dyzaWnHdIdb8VG2oGwSUcw"
 URL = "https://www.goodreads.com/book"
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = b'\x11\x1d\\>\x1a3\x806N\xb1\x1fk=\xe39\xb5'
+controller = Controller()
+
+
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
-# # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+            raise RuntimeError("DATABASE_URL is not set")
 
 
+##########################################################################################
+#Routes
 
+#Register
 @app.route("/register", methods=['GET', 'POST'])
 def sign_up():
     form = RegistrationForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            save(form.first_name.data, form.last_name.data, form.email.data, form.password.data, form.gender.data)
+            controller.save(form.first_name.data, form.last_name.data, form.email.data, form.password.data, form.gender.data)
             return redirect(url_for("login"))
 
     return render_template("pages/register.html", form=form)
 
 
-
-
-def add_session(user_name):
-    user = db.execute("select id from users where user_name = :user_name", 
-                                        {"user_name": user_name}).fetchone()
-    if user:
-        session["username"] = user_name
-        session["user_id"] = user.id
-    return False
-
-
+#Login
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if request.method=='POST':
         if form.validate_on_submit():
-            if is_credentials_valid(form.user_name.data, form.password.data):
-                add_session(form.user_name.data)
+            if controller.is_credentials_valid(form.user_name.data, form.password.data):
+                controller.add_session(form.user_name.data)
                 flash("You are successfully logged in.")
                 return redirect(url_for('index'))
             render_template("pages/login.html", form=form, error="Wrong credentials!")
     return render_template("pages/login.html", form=form)
 
+
+#Logout
 @app.route("/logout")
 def logout():
     session.pop("username", None)
@@ -71,94 +64,32 @@ def logout():
     return redirect(url_for("login"))
 
 
+
+#Index
 @app.route("/")
 def index():
     if "username" in session:    
-        if user_searched():
-            if get_searched_book(user_searched()):
-                return render_template('index.html', books=get_searched_book(user_searched()), message={"success":user_searched()})
+        if controller.user_searched():
+            if controller.get_searched_book(controller.user_searched()):
+                return render_template('index.html', books=controller.get_searched_book(controller.user_searched()), message={"success":controller.user_searched()})
             else:
-                return render_template('index.html', books=get_all_books(), message={"error":user_searched()})
-        return render_template('index.html', books=get_all_books(), session=session["username"])
+                return render_template('index.html', books=controller.get_all_books(), message={"error":controller.user_searched()})
+        return render_template('index.html', books=controller.get_all_books(), session=session["username"])
     return redirect(url_for("login"))
 
 
 
-
-def get_searched_book(search):
-    book = db.execute(f"select * from books where title ilike '%{search}%' or author ilike '%{search}%' or isbn ilike '%{search}%';").fetchall()
-    if book:
-        return book
-    return False
-
-
+#Show
 @app.route("/book/<int:id>", methods=['GET', 'POST'])
 def show(id):
     form = ReviewForm()
     if "username" in session:
         if request.method=="POST":
-            add_review(id)
+            controller.add_review(id)
             return redirect(url_for("show", id=id))
-
-        book = db.execute("select * from books where id = :id", {"id": id}).fetchone()
+        book = controller.get_book_by_id(id)
         res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": f"{KEY}", "isbns": f"{book.isbn}"})
         ratings = res.json()["books"][0]
-        print("this is reivew", is_review_submitted())
-        return render_template('pages/book.html', book=book, ratings=ratings, form =form, reviews={"all_reviews": get_reviews(), "current_user_submitted": is_review_submitted()})
+        print("this is reivew", controller.is_review_submitted())
+        return render_template('pages/book.html', book=book, ratings=ratings, form =form, reviews={"all_reviews": controller.get_reviews(), "current_user_submitted": controller.is_review_submitted()})
     return redirect(url_for("login"))
-
-
-    
-def add_review(book_id):
-    form = ReviewForm()
-    if form.validate_on_submit():
-        review = form.reviews.data
-        print(review)
-        db.execute("insert into reviews (review, rate, user_id, book_id, create_date) values (:review, :rate, :user_id, :book_id, :create_date)", 
-                                        {"review": review, "rate": "This is rate", "user_id": session["user_id"], "book_id": book_id, "create_date": "now()"})
-        db.commit()
-
-
-
-def get_reviews():
-    reviews = db.execute("select concat(first_name, ' ', last_name) as full_name, user_name, review, date_trunc('second', create_date) as create_date from users u join profiles p on p.user_id = u.id join reviews r on r.user_id = u.id order by create_date desc;").fetchall()
-    return reviews
-
-def is_review_submitted():
-    review = db.execute("select user_id from reviews where user_id = :user_id", 
-                                        {"user_id": session["user_id"]}).fetchone()
-    print(review)
-    if review:
-        return False
-    return True
-
-def get_all_books():
-    books = db.execute("select * from books limit 9 offset 1").fetchall()
-    return books
-
-def user_searched():
-    if request.args.get("search"):
-        return request.args.get("search")
-    return False
-
-def save(first_name, last_name, email, password, gender):
-    print(first_name)
-    if first_name and last_name and email and password and gender:
-        db.execute("insert into users (user_name, password) values(:user_name, :password)", {"user_name": email, "password": password})
-        user = db.execute("select id from users where user_name= :user_name", {"user_name": email}).fetchone()
-        db.execute("insert into profiles (first_name, last_name, gender, user_id) values(:first_name, :last_name, :gender, :user_id)",
-                                            {
-                                            "first_name": first_name, 
-                                            "last_name": last_name, 
-                                            "gender": gender, 
-                                            "user_id": user.id})
-        db.commit()
-
-
-def is_credentials_valid(user_name, password):
-    if user_name and password:
-        credentials = db.execute("select id from users where user_name = :user_name and password = :password", 
-                                                {"user_name": user_name, "password": password}).fetchone()
-        if credentials:
-            return True
-    return False
---
+++ controller.py
@@ -0,0 +1,130 @@
+import os
+import requests
+
+from flask import Flask, session, redirect, url_for, flash
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from flask import render_template, request
+from flask_wtf.csrf import CSRFProtect
+from forms import RegistrationForm, LoginForm, ReviewForm
+
+
+class Controller():
+
+    def __init__(self):
+        
+        #Set up database
+        self.engine = create_engine(os.getenv("DATABASE_URL"))
+        self.db = scoped_session(sessionmaker(bind=self.engine))
+    
+
+
+    #Session Management###################################################################
+    #Store Session
+    def add_session(self, user_name):
+        user = self.db.execute("select id from users where user_name = :user_name", 
+                                            {"user_name": user_name}).fetchone()
+        if user:
+            session["username"] = user_name
+            session["user_id"] = user.id
+        return False
+
+
+
+    #Books Model############################################################################
+    #Return the book searched by a user
+    def get_searched_book(self, search):
+        book = self.db.execute(f"select * from books where title ilike '%{search}%' or author ilike '%{search}%' or isbn ilike '%{search}%';").fetchall()
+        if book:
+            return book
+        return False
+
+
+
+    #Return list of all books
+    def get_all_books(self):
+        books = self.db.execute("select * from books limit 9 offset 1").fetchall()
+        return books
+
+
+
+    #Return a book by an ID
+    def get_book_by_id(self, id):
+        book = self.db.execute("select * from books where id = :id", {"id": id}).fetchone()
+        return book
+    #######################################################################################
+   
+   
+   
+    #Reviews Model#########################################################################
+    #Add a review for a book
+    def add_review(self, book_id):
+        form = ReviewForm()
+        if form.validate_on_submit():
+            review = form.reviews.data
+            print(review)
+            self.db.execute("insert into reviews (review, rate, user_id, book_id, create_date) values (:review, :rate, :user_id, :book_id, :create_date)", 
+                                            {"review": review, "rate": "This is rate", "user_id": session["user_id"], "book_id": book_id, "create_date": "now()"})
+            self.db.commit()
+
+
+
+    #Return list of all reviews
+    def get_reviews(self):
+        reviews = self.db.execute("select concat(first_name, ' ', last_name) as full_name, user_name, review, date_trunc('second', create_date) as create_date from users u join profiles p on p.user_id = u.id join reviews r on r.user_id = u.id order by create_date desc;").fetchall()
+        return reviews
+
+
+
+    #Check if user submitted a review
+    def is_review_submitted(self):
+        review = self.db.execute("select user_id from reviews where user_id = :user_id", 
+                                            {"user_id": session["user_id"]}).fetchone()
+        print(review)
+        if review:
+            return False
+        return True
+
+
+    #######################################################################################
+   
+
+
+    #Users Model###########################################################################
+    #Sign up a user
+    def save(self, first_name, last_name, email, password, gender):
+        print(first_name)
+        if first_name and last_name and email and password and gender:
+            self.db.execute("insert into users (user_name, password) values(:user_name, :password)", {"user_name": email, "password": password})
+            user = self.db.execute("select id from users where user_name= :user_name", {"user_name": email}).fetchone()
+            self.db.execute("insert into profiles (first_name, last_name, gender, user_id) values(:first_name, :last_name, :gender, :user_id)",
+                                                {
+                                                "first_name": first_name, 
+                                                "last_name": last_name, 
+                                                "gender": gender, 
+                                                "user_id": user.id})
+            self.db.commit()
+
+
+
+    #Check if user provided correct credentials
+    def is_credentials_valid(self, user_name, password):
+        if user_name and password:
+            credentials = self.db.execute("select id from users where user_name = :user_name and password = :password", 
+                                                    {"user_name": user_name, "password": password}).fetchone()
+            if credentials:
+                return True
+        return False
+
+
+    #######################################################################################
+
+
+
+    #Util##################################################################################
+    #Check if user tried to search
+    def user_searched(self):
+        if request.args.get("search"):
+            return request.args.get("search")
+        return False
